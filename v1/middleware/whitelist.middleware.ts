a436f8ba686f1fbdcba96101128503619f58b212
xMellylicious <marshm3lly@gmail.com>
2023-08-08 11:39:05 +0100

    fixed whitelist

--- v1/middleware/whitelist.middleware.ts
+++ v1/middleware/whitelist.middleware.ts
@@ -2,8 +2,8 @@ import { Request, Response, NextFunction } from "express";
 
 const whitelist = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        let validIps = ['::12', '127.0.0.1', '176.254.14.125'];
-        
+        let validIps = ['::ffff:127.0.0.1', '127.0.0.1', '176.254.14.125'];
+
         if(!validIps.includes(req.socket.remoteAddress)) {return res.status(403).json({message:"IP not whitelisted"})}
 
         next()
