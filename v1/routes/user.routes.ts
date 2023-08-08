a815061f9fc11c070d5f1b727c80ad44cf980264
xMellylicious <marshm3lly@gmail.com>
2023-08-08 11:48:33 +0100

    added in json formatting

--- v1/routes/user.routes.ts
+++ v1/routes/user.routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { hashPassword, validateToken, comparePasswords } from "../middleware/user.middleware";
 import { createUser, getInformation, createToken, updateInfo, getAll } from "../controllers/user.controller";
+import { formatUserJson } from "../middleware/format.middleware";
 
 const router = express.Router()
 
@@ -9,8 +10,8 @@ router.post('/register', hashPassword, createUser)
 router.post('/authenticate', comparePasswords, createToken)
 
 //Individual Routes
+router.get('/information/full', validateToken, formatUserJson, getAll)
 router.get('/information', validateToken, getInformation)
 router.patch('/edit', validateToken, updateInfo)
-router.get('/all', validateToken, getAll)
 
 export default router
